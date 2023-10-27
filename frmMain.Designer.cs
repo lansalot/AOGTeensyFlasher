468debc90859016ce7a5e2bd8407c13db92cf2fb
lansalot <lansalot@gmail.com>
2023-10-27 12:59:14 +0100

    Push to update CSV

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -37,23 +37,16 @@ namespace TeensyFlasher
         /// </summary>
         private void InitializeComponent()
         {
-            this.lbTeensy = new System.Windows.Forms.ListBox();
             this.lbFirmware = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.btnProgram = new System.Windows.Forms.Button();
-            this.btnREfresh = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.txtMessages = new System.Windows.Forms.TextBox();
+            this.lblMessages = new System.Windows.Forms.Label();
+            this.pbProgram = new System.Windows.Forms.ProgressBar();
+            this.lbTeensies = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
-            // 
-            // lbTeensy
-            // 
-            this.lbTeensy.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbTeensy.FormattingEnabled = true;
-            this.lbTeensy.ItemHeight = 25;
-            this.lbTeensy.Location = new System.Drawing.Point(523, 96);
-            this.lbTeensy.Name = "lbTeensy";
-            this.lbTeensy.Size = new System.Drawing.Size(358, 179);
-            this.lbTeensy.TabIndex = 0;
             // 
             // lbFirmware
             // 
@@ -62,7 +55,7 @@ namespace TeensyFlasher
             this.lbFirmware.ItemHeight = 25;
             this.lbFirmware.Location = new System.Drawing.Point(12, 96);
             this.lbFirmware.Name = "lbFirmware";
-            this.lbFirmware.Size = new System.Drawing.Size(358, 179);
+            this.lbFirmware.Size = new System.Drawing.Size(869, 229);
             this.lbFirmware.TabIndex = 1;
             this.lbFirmware.SelectedIndexChanged += new System.EventHandler(this.lbFirmware_SelectedIndexChanged);
             // 
@@ -72,51 +65,91 @@ namespace TeensyFlasher
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(13, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(274, 36);
+            this.label1.Size = new System.Drawing.Size(634, 36);
             this.label1.TabIndex = 2;
-            this.label1.Text = "1. Select a firmware";
+            this.label1.Text = "1. Select a local firmware or Refresh list if none";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(517, 25);
+            this.label2.Location = new System.Drawing.Point(13, 344);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(277, 36);
+            this.label2.Size = new System.Drawing.Size(246, 36);
             this.label2.TabIndex = 3;
-            this.label2.Text = "2. Pick your Teensy";
+            this.label2.Text = "2. Plug in Teensy";
             // 
             // btnProgram
             // 
             this.btnProgram.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnProgram.Location = new System.Drawing.Point(523, 288);
+            this.btnProgram.Location = new System.Drawing.Point(741, 335);
             this.btnProgram.Name = "btnProgram";
             this.btnProgram.Size = new System.Drawing.Size(140, 45);
             this.btnProgram.TabIndex = 4;
             this.btnProgram.Text = "Program!";
             this.btnProgram.UseVisualStyleBackColor = true;
+            this.btnProgram.Click += new System.EventHandler(this.btnProgram_Click);
             // 
-            // btnREfresh
+            // btnRefresh
             // 
-            this.btnREfresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnREfresh.Location = new System.Drawing.Point(12, 281);
-            this.btnREfresh.Name = "btnREfresh";
-            this.btnREfresh.Size = new System.Drawing.Size(140, 52);
-            this.btnREfresh.TabIndex = 5;
-            this.btnREfresh.Text = "Refresh list";
-            this.btnREfresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRefresh.Location = new System.Drawing.Point(741, 20);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(140, 52);
+            this.btnRefresh.TabIndex = 5;
+            this.btnRefresh.Text = "Refresh list";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // txtMessages
+            // 
+            this.txtMessages.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMessages.Location = new System.Drawing.Point(19, 462);
+            this.txtMessages.Multiline = true;
+            this.txtMessages.Name = "txtMessages";
+            this.txtMessages.Size = new System.Drawing.Size(862, 196);
+            this.txtMessages.TabIndex = 6;
+            // 
+            // lblMessages
+            // 
+            this.lblMessages.AutoSize = true;
+            this.lblMessages.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessages.Location = new System.Drawing.Point(13, 423);
+            this.lblMessages.Name = "lblMessages";
+            this.lblMessages.Size = new System.Drawing.Size(150, 36);
+            this.lblMessages.TabIndex = 7;
+            this.lblMessages.Text = "Messages";
+            // 
+            // pbProgram
+            // 
+            this.pbProgram.Location = new System.Drawing.Point(651, 386);
+            this.pbProgram.Name = "pbProgram";
+            this.pbProgram.Size = new System.Drawing.Size(230, 23);
+            this.pbProgram.TabIndex = 9;
+            // 
+            // lbTeensies
+            // 
+            this.lbTeensies.FormattingEnabled = true;
+            this.lbTeensies.ItemHeight = 16;
+            this.lbTeensies.Location = new System.Drawing.Point(265, 344);
+            this.lbTeensies.Name = "lbTeensies";
+            this.lbTeensies.Size = new System.Drawing.Size(356, 84);
+            this.lbTeensies.TabIndex = 10;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(913, 350);
-            this.Controls.Add(this.btnREfresh);
+            this.ClientSize = new System.Drawing.Size(913, 677);
+            this.Controls.Add(this.lbTeensies);
+            this.Controls.Add(this.pbProgram);
+            this.Controls.Add(this.lblMessages);
+            this.Controls.Add(this.txtMessages);
+            this.Controls.Add(this.btnRefresh);
             this.Controls.Add(this.btnProgram);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lbFirmware);
-            this.Controls.Add(this.lbTeensy);
             this.Name = "frmMain";
             this.Text = "agOpenGPS Teensy Flasher";
             this.Load += new System.EventHandler(this.frmMain_Load);
@@ -126,13 +159,15 @@ namespace TeensyFlasher
         }
 
         #endregion
-
-        private ListBox lbTeensy;
         private ListBox lbFirmware;
         private Label label1;
         private Label label2;
         private Button btnProgram;
-        private Button btnREfresh;
+        private Button btnRefresh;
+        private TextBox txtMessages;
+        private Label lblMessages;
+        private ProgressBar pbProgram;
+        private ListBox lbTeensies;
     }
 }
 
--- frmMain.cs
+++ frmMain.cs
@@ -1,22 +1,60 @@
-﻿using System;
+﻿using libTeensySharp;
+using lunOptics.libTeensySharp;
+using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace TeensyFlasher
 {
     public partial class frmMain : Form
     {
+        TeensyWatcher watcher;
+        String localCSV = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Firmwares.csv");
+        string localHexStub = AppDomain.CurrentDomain.BaseDirectory;
+        string chosenFirmware = "";
         public frmMain()
         {
             InitializeComponent();
+            watcher = new TeensyWatcher(SynchronizationContext.Current);
+            watcher.ConnectedTeensies.CollectionChanged += ConnectedTeensiesChanged;
+            foreach (var teensy in watcher.ConnectedTeensies)
+            {
+                lbTeensies.Items.Add(teensy);
+            }
+            if (lbTeensies.Items.Count > 0) lbTeensies.SelectedIndex = 0;
         }
 
+        private void ConnectedTeensiesChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    foreach (var teensy in e.NewItems)
+                    {
+                        lbTeensies.Items.Add(teensy);
+                    }
+                    break;
+
+                case NotifyCollectionChangedAction.Remove:
+                    foreach (var teensy in e.OldItems)
+                    {
+                        lbTeensies.Items.Remove(teensy);
+                    }
+                    break;
+            }
+            if (lbTeensies.SelectedIndex == -1 && lbTeensies.Items.Count > 0) lbTeensies.SelectedIndex = 0;
+        }
         private void frmMain_Load(object sender, EventArgs e)
         {
             btnProgram.Enabled = false;
@@ -24,7 +62,54 @@ namespace TeensyFlasher
 
         private void lbFirmware_SelectedIndexChanged(object sender, EventArgs e)
         {
+            var lines = File.ReadAllLines(localCSV);
+            var secondColumn = lines
+                .Where(line => line.Split(',')[0] == (String)lbFirmware.SelectedValue)
+                .Select(line => line.Split(',')[1])
+                .ToList();
 
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string url = "https://raw.githubusercontent.com/lansalot/AOGTeensyFlasher/main/Firmwares.csv";
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFile(url, localCSV);
+                }
+                txtMessages.Text += "Firmware list downloaded!\r\n";
+
+                // Read the CSV file and fill the listbox with the first column
+                var lines = File.ReadAllLines(localCSV);
+                var firstColumn = lines.Select(line => line.Split(',')[0]).ToList();
+                lbFirmware.DataSource = firstColumn;
+
+            }
+            catch
+            {
+                txtMessages.Text += "Error downloading firmware list\r\n";
+            }
+        }
+
+        private void btnProgram_Click(object sender, EventArgs e)
+        {
+            var teensy = lbTeensies.SelectedItem as ITeensy;
+            //if (teensy != null)
+            //{
+            //    string filename = tbHexfile.Text;
+            //    if (File.Exists(filename))
+            //    {
+            //        var progress = new Progress<int>(v => progressBar.Value = v);
+            //        progressBar.Visible = true;
+            //        var result = await teensy.UploadAsync(filename, progress);
+            //        MessageBox.Show(result.ToString(), "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //        progressBar.Visible = false;
+            //        progressBar.Value = 0;
+            //    }
+            //    else MessageBox.Show("File does not exist", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //}
         }
     }
 }
