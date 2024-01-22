02709daa4089ca325d5826b9a6701a6ed66c8425
lansalot <lansalot@gmail.com>
2024-01-22 21:13:57 +0000

    WIP for adding F9P

--- TeensyFlasher/frmMain.Designer.cs
+++ TeensyFlasher/frmMain.Designer.cs
@@ -37,139 +37,186 @@ namespace TeensyFlasher
         /// </summary>
         private void InitializeComponent()
         {
-            this.lbFirmware = new System.Windows.Forms.ListBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.btnProgram = new System.Windows.Forms.Button();
-            this.btnRefresh = new System.Windows.Forms.Button();
-            this.txtMessages = new System.Windows.Forms.TextBox();
-            this.lblMessages = new System.Windows.Forms.Label();
-            this.pbProgram = new System.Windows.Forms.ProgressBar();
+            this.tbPages = new System.Windows.Forms.TabControl();
+            this.tabTeensy = new System.Windows.Forms.TabPage();
             this.lbTeensies = new System.Windows.Forms.ListBox();
+            this.pbProgram = new System.Windows.Forms.ProgressBar();
+            this.lblMessages = new System.Windows.Forms.Label();
+            this.txtMessages = new System.Windows.Forms.TextBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnProgram = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbFirmware = new System.Windows.Forms.ListBox();
+            this.tabGPS = new System.Windows.Forms.TabPage();
+            this.tbPages.SuspendLayout();
+            this.tabTeensy.SuspendLayout();
             this.SuspendLayout();
             // 
-            // lbFirmware
+            // tbPages
             // 
-            this.lbFirmware.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbFirmware.FormattingEnabled = true;
-            this.lbFirmware.ItemHeight = 25;
-            this.lbFirmware.Location = new System.Drawing.Point(12, 96);
-            this.lbFirmware.Name = "lbFirmware";
-            this.lbFirmware.Size = new System.Drawing.Size(869, 229);
-            this.lbFirmware.TabIndex = 1;
-            this.lbFirmware.SelectedIndexChanged += new System.EventHandler(this.lbFirmware_SelectedIndexChanged);
+            this.tbPages.Controls.Add(this.tabTeensy);
+            this.tbPages.Controls.Add(this.tabGPS);
+            this.tbPages.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPages.Location = new System.Drawing.Point(13, 51);
+            this.tbPages.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbPages.Name = "tbPages";
+            this.tbPages.SelectedIndex = 0;
+            this.tbPages.Size = new System.Drawing.Size(971, 719);
+            this.tbPages.TabIndex = 11;
             // 
-            // label1
+            // tabTeensy
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(13, 25);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(634, 36);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "1. Select a local firmware or Refresh list if none";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(13, 344);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(112, 36);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Teensy";
-            // 
-            // btnProgram
-            // 
-            this.btnProgram.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnProgram.Location = new System.Drawing.Point(741, 335);
-            this.btnProgram.Name = "btnProgram";
-            this.btnProgram.Size = new System.Drawing.Size(140, 45);
-            this.btnProgram.TabIndex = 4;
-            this.btnProgram.Text = "Program!";
-            this.btnProgram.UseVisualStyleBackColor = true;
-            this.btnProgram.Click += new System.EventHandler(this.btnProgram_Click);
-            // 
-            // btnRefresh
-            // 
-            this.btnRefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRefresh.Location = new System.Drawing.Point(741, 20);
-            this.btnRefresh.Name = "btnRefresh";
-            this.btnRefresh.Size = new System.Drawing.Size(140, 52);
-            this.btnRefresh.TabIndex = 5;
-            this.btnRefresh.Text = "Refresh list";
-            this.btnRefresh.UseVisualStyleBackColor = true;
-            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
-            // 
-            // txtMessages
-            // 
-            this.txtMessages.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMessages.Location = new System.Drawing.Point(19, 462);
-            this.txtMessages.Multiline = true;
-            this.txtMessages.Name = "txtMessages";
-            this.txtMessages.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtMessages.Size = new System.Drawing.Size(862, 196);
-            this.txtMessages.TabIndex = 6;
-            // 
-            // lblMessages
-            // 
-            this.lblMessages.AutoSize = true;
-            this.lblMessages.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMessages.Location = new System.Drawing.Point(13, 423);
-            this.lblMessages.Name = "lblMessages";
-            this.lblMessages.Size = new System.Drawing.Size(150, 36);
-            this.lblMessages.TabIndex = 7;
-            this.lblMessages.Text = "Messages";
-            // 
-            // pbProgram
-            // 
-            this.pbProgram.Location = new System.Drawing.Point(651, 386);
-            this.pbProgram.Name = "pbProgram";
-            this.pbProgram.Size = new System.Drawing.Size(230, 23);
-            this.pbProgram.TabIndex = 9;
+            this.tabTeensy.Controls.Add(this.lbTeensies);
+            this.tabTeensy.Controls.Add(this.pbProgram);
+            this.tabTeensy.Controls.Add(this.lblMessages);
+            this.tabTeensy.Controls.Add(this.txtMessages);
+            this.tabTeensy.Controls.Add(this.btnRefresh);
+            this.tabTeensy.Controls.Add(this.btnProgram);
+            this.tabTeensy.Controls.Add(this.label2);
+            this.tabTeensy.Controls.Add(this.label1);
+            this.tabTeensy.Controls.Add(this.lbFirmware);
+            this.tabTeensy.Location = new System.Drawing.Point(4, 45);
+            this.tabTeensy.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabTeensy.Name = "tabTeensy";
+            this.tabTeensy.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabTeensy.Size = new System.Drawing.Size(963, 670);
+            this.tabTeensy.TabIndex = 0;
+            this.tabTeensy.Text = "Teensy";
+            this.tabTeensy.UseVisualStyleBackColor = true;
             // 
             // lbTeensies
             // 
             this.lbTeensies.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbTeensies.FormattingEnabled = true;
             this.lbTeensies.ItemHeight = 25;
-            this.lbTeensies.Location = new System.Drawing.Point(159, 344);
+            this.lbTeensies.Location = new System.Drawing.Point(133, 286);
+            this.lbTeensies.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.lbTeensies.Name = "lbTeensies";
-            this.lbTeensies.Size = new System.Drawing.Size(475, 79);
-            this.lbTeensies.TabIndex = 10;
+            this.lbTeensies.Size = new System.Drawing.Size(475, 54);
+            this.lbTeensies.TabIndex = 19;
+            // 
+            // pbProgram
+            // 
+            this.pbProgram.Location = new System.Drawing.Point(711, 351);
+            this.pbProgram.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pbProgram.Name = "pbProgram";
+            this.pbProgram.Size = new System.Drawing.Size(229, 23);
+            this.pbProgram.TabIndex = 18;
+            // 
+            // lblMessages
+            // 
+            this.lblMessages.AutoSize = true;
+            this.lblMessages.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessages.Location = new System.Drawing.Point(13, 338);
+            this.lblMessages.Name = "lblMessages";
+            this.lblMessages.Size = new System.Drawing.Size(150, 36);
+            this.lblMessages.TabIndex = 17;
+            this.lblMessages.Text = "Messages";
+            // 
+            // txtMessages
+            // 
+            this.txtMessages.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMessages.Location = new System.Drawing.Point(19, 382);
+            this.txtMessages.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtMessages.Multiline = true;
+            this.txtMessages.Name = "txtMessages";
+            this.txtMessages.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMessages.Size = new System.Drawing.Size(921, 196);
+            this.txtMessages.TabIndex = 16;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRefresh.Location = new System.Drawing.Point(800, 18);
+            this.btnRefresh.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(140, 52);
+            this.btnRefresh.TabIndex = 15;
+            this.btnRefresh.Text = "Refresh list";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // btnProgram
+            // 
+            this.btnProgram.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProgram.Location = new System.Drawing.Point(800, 282);
+            this.btnProgram.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnProgram.Name = "btnProgram";
+            this.btnProgram.Size = new System.Drawing.Size(140, 46);
+            this.btnProgram.TabIndex = 14;
+            this.btnProgram.Text = "Program!";
+            this.btnProgram.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 292);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 44);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Teensy";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(634, 36);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "1. Select a local firmware or Refresh list if none";
+            // 
+            // lbFirmware
+            // 
+            this.lbFirmware.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbFirmware.FormattingEnabled = true;
+            this.lbFirmware.ItemHeight = 25;
+            this.lbFirmware.Location = new System.Drawing.Point(21, 74);
+            this.lbFirmware.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lbFirmware.Name = "lbFirmware";
+            this.lbFirmware.Size = new System.Drawing.Size(919, 204);
+            this.lbFirmware.TabIndex = 11;
+            // 
+            // tabGPS
+            // 
+            this.tabGPS.Location = new System.Drawing.Point(4, 45);
+            this.tabGPS.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabGPS.Name = "tabGPS";
+            this.tabGPS.Size = new System.Drawing.Size(1120, 783);
+            this.tabGPS.TabIndex = 1;
+            this.tabGPS.Text = "Ublox";
+            this.tabGPS.UseVisualStyleBackColor = true;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(913, 677);
-            this.Controls.Add(this.lbTeensies);
-            this.Controls.Add(this.pbProgram);
-            this.Controls.Add(this.lblMessages);
-            this.Controls.Add(this.txtMessages);
-            this.Controls.Add(this.btnRefresh);
-            this.Controls.Add(this.btnProgram);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.lbFirmware);
+            this.ClientSize = new System.Drawing.Size(1229, 1426);
+            this.Controls.Add(this.tbPages);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "frmMain";
             this.Text = "agOpenGPS Teensy Flasher";
             this.Load += new System.EventHandler(this.frmMain_Load);
+            this.tbPages.ResumeLayout(false);
+            this.tabTeensy.ResumeLayout(false);
+            this.tabTeensy.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private ListBox lbFirmware;
-        private Label label1;
-        private Label label2;
-        private Button btnProgram;
-        private Button btnRefresh;
-        private TextBox txtMessages;
-        private Label lblMessages;
-        private ProgressBar pbProgram;
+        private TabControl tbPages;
+        private TabPage tabTeensy;
+        private TabPage tabGPS;
         private ListBox lbTeensies;
+        private ProgressBar pbProgram;
+        private Label lblMessages;
+        private TextBox txtMessages;
+        private Button btnRefresh;
+        private Button btnProgram;
+        private Label label2;
+        private Label label1;
+        private ListBox lbFirmware;
     }
 }
 
