fc5ea9f15d44847abf539792a64d411335258f31
lansalot <lansalot@gmail.com>
2024-02-12 23:08:22 +0000

    Just triggering to test pipeline

--- TeensyFlasher/Program.cs
+++ TeensyFlasher/Program.cs
@@ -17,7 +17,6 @@ namespace TeensyFlasher
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmMain());
-            
         }
     }
 }
